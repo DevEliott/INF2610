6135a9fa632402bd0f3ae2020ed3ea4a637ca9c4
eliott <dev.eliottdev@gmail.com>
2022-02-04 12:31:34 -0500

    q1.2 je sais pas si c bon

--- TP1/part1.c
+++ TP1/part1.c
@@ -14,8 +14,34 @@
 
 void question1(int choix)
 {
-    printf("75dbcb01f571f1c32e196c3a7d27f62e (printed using printf)");
+    const char printfMsg[] = "75dbcb01f571f1c32e196c3a7d27f62e (printed using printf)";
     const char writeMsg[] = "75dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n";
-    write(1, writeMsg, sizeof(writeMsg));
-    printf("\n");
+    if (choix == 1)
+    {
+        for (int i = 0; i < strlen(printfMsg); i++)
+        {
+            char c = printfMsg[i];
+            printf("%c", c);
+        }
+        write(1, writeMsg, strlen(writeMsg));
+        printf("\n");
+    }
+    else if (choix == 2)
+    {
+        FILE *file = fopen("./output2.txt", "w");
+        if (file == NULL)
+        {
+            perror("fopen");
+            exit(EXIT_FAILURE);
+        }
+        if (setvbuf(file, NULL, _IONBF, BUFSIZ) != 0)
+        {
+            perror("setvbuf failed");
+            exit(EXIT_FAILURE);
+        }
+        printf("%s", printfMsg);
+        // setvbuf(file, NULL, _IONBF, BUFSIZ);
+        write(1, writeMsg, strlen(writeMsg));
+        fclose(file);
+    }
 }
