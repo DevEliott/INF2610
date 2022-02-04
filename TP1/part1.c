f5932bd09186ab06ecd5145ae3c3b8d64bcf2cf9
eliott <dev.eliottdev@gmail.com>
2022-02-04 11:12:23 -0500

    q1.1

--- TP1/part1.c
+++ TP1/part1.c
@@ -1,19 +1,21 @@
 /*
  *  part1.c
- * 
+ *
  * ajoutez vos noms, prénoms, matricules et votre section de laboratoire
  */
 
 // TODO
 // Si besoin, ajouter ici les directives d'inclusion
 // -------------------------------------------------
-
+#include <stdio.h>
 // -------------------------------------------------
 
 #include "./libprocesslab/libprocesslab.h"
 
-void question1(int choix) {
-    // TODO
- 
+void question1(int choix)
+{
+    printf("75dbcb01f571f1c32e196c3a7d27f62e (printed using printf)");
+    const char writeMsg[] = "75dbcb01f571f1c32e196c3a7d27f62e (printed using write)\n";
+    write(1, writeMsg, sizeof(writeMsg));
+    printf("\n");
 }
-    
